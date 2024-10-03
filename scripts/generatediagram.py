e5a2c3bfd845d1a79a9b18605ef364cbce739ea0
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-10-03 11:09:15 -0500

    Update generatediagram.py

--- scripts/generatediagram.py
+++ scripts/generatediagram.py
@@ -5,18 +5,18 @@ from diagrams.aws.database import RDS
 import datetime
 import os
 
-# Get the current date in YYYY-MM-DD format
-current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+# Get the current date and time in YYYY-MM-DD-HH-MM-SS format
+current_datetime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
 # Define the output directory and filename
 output_directory = 'output'
-output_file = f"{output_directory}/output-{current_date}.png"
+output_file = f"{output_directory}/output-{current_datetime}.png"
 
-# Create the 'Output' directory if it doesn't exist
+# Create the 'output' directory if it doesn't exist
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-# Generate the diagram and save it in the 'Output' directory with the new file name
+# Generate the diagram and save it in the 'output' directory with the new file name
 with Diagram("Simple Web Service", show=False, filename=output_file):
     ELB("lb") >> EC2("web") >> RDS("db")
 
